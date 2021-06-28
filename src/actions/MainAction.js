4b78e4cef383f43cdbb55274b1794128ed82b291
altiparmakzeynep <zzeynepaltiparmak@gmail.com>
2021-06-28 23:41:21 +0300

    updated post photos with API

--- src/actions/MainAction.js
+++ src/actions/MainAction.js
@@ -16,18 +16,27 @@ export const selectPhotos = (value) => {
         payload: value
     }
 }
-export const uploadPhotos = (value) => {
+export const uploadPhotos = (formData) => {
     return dispatch => {
         dispatch({
             type: UPLOAD_PHOTOS 
         })
-        console.log(value);
+        console.log(formData);
         axios({
             method: "POST",
             url: "http://192.168.1.6:5003/api/v1/posts",
+            data: formData,
             headers: {
                 'Authorization': "Bearer  "  +  "D3UEv58RQ2KN89cpa6Q9kyxmctD3rlCD7nHQ5DwrGpENscSzUgH8lTLQ5I3pLBFgrF2iCJPxxgkBUh5KZBSq62aXmRSLtpSas2QfQPx4oMV1heagVImVoPbkTFMrRXtitI691TxBQ1UWxVnWqsfwPwrLIz4Vra5EiaFskJBlpy3cTtIFSeiMRTZyO8g1Z34PcW8iB6ZaWRPvaERBnFwfiTUm1uz7Cdv5FUL0uKO8hU5SbvzmlgoLwpLdpW",
             }           
+        }).then((result) => {
+            if(result.data.status == "success"){
+                dispatch({
+                    payload: result.data.data
+                })
+            }
+        }).catch((err) => {
+            console.log("err",err);
         })
     } 
 }
